f5bdbb7d980cc4124fba46e3e0f444e2738c3c02
MochammadRizkyFahrezy <90676299+MochammadRizkyFahrezy@users.noreply.github.com>
2021-11-06 23:27:22 +0700

    Add files via upload

---
+++ batagor.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="batagor.css">
+</head>
+<body>
+    <div id="nam"><p >BATAGOR <br>
+        </p></div>
+    <a href="utama.php"><div id="back">
+         Homepage
+    </div></a>
+    <div class="scroll">
+        <h2>Resep Makanan</h2>
+        <ul>
+            <li>Tahu putih 2 buah, potong menjadi 8 bagian</li>
+            <li>kulit pangsit 8 lembar</li> 
+            <li>Ikan tenggiri fillet 200 g, haluskan</li>  
+            <li>Daun bawang 3 batang, cincang halus</li>
+            <li>Garam 1 sdt</li>
+            <li>Merica ½ sdt</li>
+            <li>Gula pasir 1 sdt</li>
+            <li>Putih telur 2 butir</li>
+            <li>Telur 2 butir</li>
+            <li>Air dingin 150 ml</li>
+            <li>Tepung sagu 100 g</li>
+            <li>Tepung terigu 20 g</li>
+
+        </ul>
+        <h2>Cara Membuat</h2>
+        <ol>
+        <li>Siapkan wadah bersih untuk menguleni adonan.</li>    
+        <li>Campurkan ikan tenggiri yang sudah dihaluskan, daun bawang, garam, dan gula pasir. Campur dengan menggunakan tangan atau spatula hingga tercampur rata.</li> 
+        <li>Masukkan putih telur kemudian aduk.</li> 
+        <li>Tuang sedikit demi sedikit air dingin sembari terus menguleni adonan.</li> 
+        <li>Masukkan tepung sagu sedikit demi sedikit sembari terus diaduk, kemudian masukkan tepung sagu. Aduk adonan hingga rata.</li> 
+        <li>Kukus tahu terlebih dahulu.</li> 
+        <li>Iris bagian tengah tahu atau kosongkan bagian tengah tahu agar bisa diisi dengan adonan.</li> 
+        <li>Masukkan adonan ke dalam tahu juga taruh adonan di tengah kulit pangsit. Tutup kulit pangsit. Sisihkan.</li> 
+        <li>Kocok 2 butir telur dan beri sedikit garam.</li> 
+        <li>Tahu dan kulit pangsit yang sudah diberi adonan kemudian masukkan ke dalam telur.</li> 
+        <li>Siapkan penggorengan. Goreng batagor dalam minyak panas dengan nyala api sedang.</li> 
+        <li>Goreng batagor hingga berwarna coklat keemasan atau matang.</li> 
+        <li>Sajikan batagor dengan saus kacang, kecap, sambal, dan air perasan jeruk nipis.</li> 
+
+
+        </ol>
+    </div>
+    <div id ="img-area">
+
+    </div>
+    
+</body>
+</html>
