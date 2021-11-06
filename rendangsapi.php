210605536aa09fe505099393263a9f20d7963544
JerryLivano <90676468+JerryLivano@users.noreply.github.com>
2021-11-06 21:53:41 +0700

    Create rendangsapi.php

---
+++ rendangsapi.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200;0,400;1,300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="rendangsapi.css">
+</head>
+<body>
+    <a href="utama.php"><img src="assets/back.png" alt="back" id="back"></a>
+
+    <div class="all">
+        <div class="scroll">       
+        <h1>Resep Rendang Sapi</h1>
+        <hr><br>
+
+        <div id="display">
+        <div id="a">
+        <h2>Bahan</h2>
+        <ul class="bahan">
+            <li>500 gr daging</li>
+            <li>1 batang serai geprek</li>
+            <li>3 lembar daun jeruk</li>
+            <li>2 lembar daun salam</li>
+            <li>1 sdt kaldu sapi dan garam</li>
+            <li>1/2 sdt jintan bubuk</li>
+            <li>1/2 sdt merica bubuk</li>
+            <li>1 ruas lengkuas geprek</li>
+            <li>2 cm kayu manis</li>
+            <li>1 sdt gula merah</li>
+            <li>Jeruk nipis</li>
+            <li>500 ml santan dari 1/2 kelapa</li>
+        </ul>
+        </div>
+
+            <img src="assets/rendang.jpg" alt="rendang" id="b">
+
+        <div id="c">
+        <h2>Bumbu Halus</h2>
+        <ul class="bahan">
+            <li>4 siung bawang putih</li>
+            <li>6 butir bawang merah</li>
+            <li>2 butir kemiri</li>
+            <li>1/2 sdt ketumbar</li>
+            <li>3 cabai merah besar (buang bijinya)</li>
+            <li>2 cm kunyit</li>
+            <li>3 cm jahe</li>
+        </ul>
+        </div>
+        </div>
+
+        <div id="d">
+        <h2>Cara Membuat</h2>
+        <ol class="bahan">
+            <li>Langkah pertama, cuci bersih daging, potong sesuai selera, beri perasan jeruk nipis, lalu cuci lagi.</li>
+            <li>Setelah itu, haluskan bumbu, lalu tumis bumbu halus dengan serai, daun jeruk, daun salam, kayu manis dan lengkuas sampai matang.</li>
+            <li>Masukkan daging, aduk rata, lalu masukkan santan. Masak dengan api kecil sambil sesekali diaduk.</li>
+            <li>Kemudian masukkan garam, kaldu, jintan, merica dan gula merah, diamkan hingga bumbu meresap dan airnya menyusut.</li>
+            <li>Koreksi rasa, angkat dan hidangkan.</li>
+        </ol>
+        </div>
+
+        <marquee id="sumber">Sumber : <i>merdeka.com</i></marquee>
+
+        </div>
+    </div>
+</body>
+</html>
