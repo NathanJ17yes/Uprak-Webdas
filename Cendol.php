e4f55bda8e92c0b45ed39dbde3bfff2d4c4ef86b
NathanJ17yes <90676004+NathanJ17yes@users.noreply.github.com>
2021-11-06 21:16:21 +0700

    Update Cendol.php

--- Cendol.php
+++ Cendol.php
@@ -4,15 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Cendol</title>
     <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200;0,400;1,300&display=s
     wap" rel="stylesheet">
     <link rel="stylesheet" href="Styling cendol.css"/>
 </head>
 <body>
-    <div id="back">
+   <a href="utama.php"><div id="back">
          Homepage
-    </div>
+    </div></a>
     <div id="jud"><p >CENDOL <br>
         DAWET</p></div>
     <div class="scroll">
