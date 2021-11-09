06028e4f649c8004d5903926a027012f1e8802e2
JerryLivano <90676468+JerryLivano@users.noreply.github.com>
2021-11-09 19:01:18 +0700

    Create credit.php

---
+++ credit.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="credit.css">
+</head>
+<body>
+<div id="all">
+    <div id="credit">
+        <p id="judul">Credits</p>
+        <hr>
+            <marquee direction="up" scrollamount="10">
+            <p id="sub">Resep Cendol</p>
+            https://www.merdeka.com/trending/5-cara-membuat-cendol-dawet-segar-ala-rumahan-bikin-nagih-tanpa-ambyar-kln.html
+            <br><br>
+            <p id="sub">Resep Batagor</p>
+            https://m.merdeka.com/gaya/resep-dan-cara-membuat-batagor-bandung-murah-dan-sederhana-serta-mudah-di-rumah-kln.html
+            <br><br>
+            <p id="sub">Resep Rendang</p>
+            https://www.merdeka.com/jateng/5-resep-bumbu-rendang-daging-sapi-yang-enak-dan-lezat-mudah-dibuat-kln.html
+            <br><br>
+            <p id="sub">Gambar Cendol</p>
+            https://fibercreme.com/wp-content/uploads/2021/08/cendol-dawet.jpg
+            <br><br>
+            <p id="sub">Gambar Batagor</p>
+            https://tokomesin.id/wp-content/uploads/2018/04/Batagor-Telur.jpg
+            <br><br>
+            <p id="sub">Gambar Rendang</p>
+            https://www.teahub.io/photos/full/138-1381938_food-rendang.jpg <br>
+            https://sweetrip.id/wp-content/uploads/2020/05/rendangmamaelin_100497628_105737341071963_4076405930119227622_n.jpg
+            <br><br>
+            <p id="sub">Gambar Background Form</p>
+            https://id.pinterest.com/pin/117515871519659101/
+            <br><br>
+            <p id="sub">Gambar Background Credits</p>
+            https://www.freepptbackgrounds.net/wp-content/uploads/2020/03/Breakfast-food-Backgrounds.jpg
+            </marquee>
+    </div>
+    <a href="utama.php"><div id="utama">Main Page</div></a>
+</div>    
+</body>
+</html>
